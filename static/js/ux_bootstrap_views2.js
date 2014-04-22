f26829f2e08d74641bf009d830744673085a7b4c
Carlos Hinojosa <carloscience@gmail.com>
2014-04-22 18:57:22 -0400

    added jquery datepicker

--- static/js/ux_bootstrap_views2.js
+++ static/js/ux_bootstrap_views2.js
@@ -51,7 +51,7 @@ IONUX.Views.Left = Backbone.View.extend({
 			if ($(this).is(':visible')) {
             	$link.removeClass('arrow_down').addClass('arrow_right');              
         	} else {
-        		$link.removeClass('arrow_down').addClass('arrow_right');
+        		$link.removeClass('arrow_right').addClass('arrow_down');
        		}        
 		});
 	},
